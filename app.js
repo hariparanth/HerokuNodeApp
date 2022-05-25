b6a7abd1b8cea660f07772ad0d5608bf86f49582
hariparanth <harigitofficial@gmail.com>
2022-05-25 22:19:47 +0530

    added dynamic port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+const port=process.env.port || 3000
 app.get('/', function(req, res){
    res.send("Hello World!");
 });
@@ -9,4 +9,4 @@ app.post('/hello', function(req, res){
    res.send("You just called the post method at '/hello'!\n");
 });
 
-app.listen(3000);
+app.listen(port);
